9e6bf1f20cdf18511e627c13ac0b08b3dec5df84
libra146 <1462326016@qq.com>
2023-07-19 22:15:21 +0800

    feature: add balanced-binary-tree.py

--- binary_tree/balanced-binary-tree.py
+++ binary_tree/balanced-binary-tree.py
@@ -19,3 +19,24 @@ class Solution:
         if right_depth == -1:
             return -1
         return max(left_depth, right_depth) + 1 if abs(left_depth - right_depth) < 2 else -1
+
+
+class Solution2:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+        res = True
+
+        def dfs(node):
+            nonlocal res
+            if node is None:
+                return 0
+            left_depth = dfs(node.left)
+            right_depth = dfs(node.right)
+            if abs(left_depth - right_depth) > 1:
+                res = False
+            return max(left_depth, right_depth) + 1
+
+        l = dfs(root.left)
+        r = dfs(root.right)
+        return all([res, abs(l - r) <= 1])
