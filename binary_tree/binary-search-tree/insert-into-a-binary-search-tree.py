4e21336a7eb96836460103127335e754bc8e26e2
libra146 <1462326016@qq.com>
2023-08-15 10:26:33 +0800

    feature: add insert-into-a-binary-search-tree.py

---
+++ binary_tree/binary-search-tree/insert-into-a-binary-search-tree.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from utils import TreeNode
+
+
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None:
+            return TreeNode(val)
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
