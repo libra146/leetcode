61a5cab095ca44fb75ff700ba2fba223be7a7fce
libra146 <1462326016@qq.com>
2023-08-15 11:30:52 +0800

    feature: add validate-binary-search-tree.py

---
+++ binary_tree/binary-search-tree/validate-binary-search-tree.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from utils import TreeNode
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.isValid(root, None, None)
+
+    def isValid(self, root: Optional[TreeNode], max_val, min_val):
+        if root is None:
+            return True
+        if max_val is not None and root.val < max_val.val:
+            return False
+        if min_val is not None and root.val > min_val.val:
+            return False
+        return self.isValid(root.left, max_val, root) and self.isValid(root.right, root, min_val)
