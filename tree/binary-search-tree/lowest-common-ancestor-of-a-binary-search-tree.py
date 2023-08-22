1d7f36863e280e95bd5469bc9c4bab5347cfd242
libra146 <1462326016@qq.com>
2023-08-22 23:21:25 +0800

    feature: add lowest-common-ancestor-of-a-binary-search-tree.py

---
+++ tree/binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,25 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return
+        if root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if not left and not right:
+            return root
+        if not left and not right:
+            return
+        return left if left else right
+
+
+class Solution2:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        cur = root
+        while cur:
+            if cur.val > p.val and cur.val > q.val:
+                cur = cur.left
+            elif cur.val < p.val and cur.val < q.val:
+                cur = cur.right
+            else:
+                return cur
