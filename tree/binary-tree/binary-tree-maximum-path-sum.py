171935d208c2fd9ec44663a7abd759359d43de8f
libra146 <1462326016@qq.com>
2023-08-22 23:50:03 +0800

    feature: add binary-tree-maximum-path-sum.py

---
+++ tree/binary-tree/binary-tree-maximum-path-sum.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from utils import TreeNode
+
+
+class Solution:
+
+    def __init__(self):
+        self.res = float('-inf')
+
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        self.dfs(root)
+        return self.res
+
+    def dfs(self, root):
+        if not root:
+            return 0
+        left = self.dfs(root.left)
+        right = self.dfs(root.right)
+        # 因为要取最大值，所以负值要过滤掉
+        left = left if left > 0 else 0
+        right = right if right > 0 else 0
+        self.res = max(self.res, root.val + left + right)
+        return root.val + max(left, right)
