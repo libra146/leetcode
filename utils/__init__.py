00b7011fbfa07baa3c9ff890c60ca88f38b771e0
libra146 <1462326016@qq.com>
2023-08-29 16:02:45 +0800

    feature: fix create_tree

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,38 +3,33 @@ import ast
 from utils.TreeNode import TreeNode
 
 
-def create_tree(root):
-    if isinstance(root, str):
-        root = ast.literal_eval(root.replace('null', 'None'))
+def create_tree(node_list):
+    if isinstance(node_list, str):
+        node_list = ast.literal_eval(node_list.replace('null', 'None'))
+    if not node_list:
+        return TreeNode()
+    if len(node_list) == 1:
+        return TreeNode(node_list[0])
     index = 1
-    layer = [root[0]]
-    root_node = None
-    sub_node = []
-    while layer:
-        # print(layer)
-        if root_node is None:
-            root_node = TreeNode(val=layer[0])
-            sub_node.append(root_node)
-        else:
-            sub_node_node = []
-            for i, a in enumerate(sub_node):
-                left_value, right_value = layer[i * 2:i * 2 + 2]
-                if left_value is None:
-                    sub_node_node.append(left_value)
-                else:
-                    a.left = TreeNode(val=left_value)
-                    sub_node_node.append(a.left)
-                if right_value is None:
-                    sub_node_node.append(right_value)
-                else:
-                    a.right = TreeNode(val=right_value)
-                    sub_node_node.append(a.right)
-            sub_node = sub_node_node
-        layer = root[pow(2, index) - 1:pow(2, index + 1) - 1]
+    root = TreeNode(node_list[0])
+    q = [root]
+    while q:
+        if index >= len(node_list):
+            break
+        r = q.pop(0)
+        r.left = TreeNode(val=node_list[index]) if node_list[index] else None
         index += 1
-    return root_node
+        r.right = TreeNode(val=node_list[index]) if node_list[index] else None
+        index += 1
+        if r.left:
+            q.append(r.left)
+        if r.right:
+            q.append(r.right)
+    return root
 
 
 if __name__ == '__main__':
     # create_tree('[1,null,2]')
-    create_tree('[3,9,20,null,null,15,7]')
+    print(create_tree('[3,9,20,null,null,15,7]'))
+    print(create_tree('[1,2,3,4,5,6,null,null,null,7,8]'))
+    print(create_tree('[1,3,2,null,6,4,5,null,null,null,null,8,7]'))
