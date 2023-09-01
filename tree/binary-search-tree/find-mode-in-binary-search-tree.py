8de9cac2d04105668c242719c4e36109a16c58d9
libra146 <1462326016@qq.com>
2023-09-01 23:01:33 +0800

    feature: add find-mode-in-binary-search-tree.py

--- tree/binary-search-tree/find-mode-in-binary-search-tree.py
+++ tree/binary-search-tree/find-mode-in-binary-search-tree.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from utils import TreeNode
+from utils import TreeNode, create_tree
 
 
 class Solution:
@@ -21,3 +21,36 @@ class Solution:
         traverse(root)
         value = max(hash_map.values())
         return [a for a in hash_map.keys() if hash_map[a] == value]
+
+
+class Solution2:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        max_count = 0
+        current_val = None
+        current_count = 0
+
+        def traverse(node):
+            nonlocal current_count, current_val, max_count
+            if not node:
+                return
+            traverse(node.left)
+            if current_val == node.val:
+                current_count += 1
+            else:
+                current_val = node.val
+                current_count = 1
+            if current_count > max_count:
+                res.clear()
+                max_count = current_count
+                res.append(current_val)
+            elif current_count == max_count:
+                res.append(current_val)
+            traverse(node.right)
+
+        traverse(root)
+        return res
+
+
+if __name__ == '__main__':
+    print(Solution2().findMode(create_tree('[1,null,2]')))
