0cae74c408b5c2b8c5eeee312875833ab6cf52bd
libra146 <1462326016@qq.com>
2023-09-10 21:22:07 +0800

    feature: fix second-minimum-node-in-a-binary-tree.py

--- tree/binary-tree/second-minimum-node-in-a-binary-tree.py
+++ tree/binary-tree/second-minimum-node-in-a-binary-tree.py
@@ -17,3 +17,21 @@ class Solution:
         v = list(self.res.keys())
         v.sort()
         return v[1] if len(v) > 1 else None
+
+
+class Solution2:
+
+    def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
+        if root.left is None and root.left is None:
+            return -1
+        left = root.left.val
+        right = root.right.val
+        if root.val == root.right.val:
+            right = self.findSecondMinimumValue(root.right)
+        if root.val == root.left.val:
+            left = self.findSecondMinimumValue(root.left)
+        if right == -1:
+            return left
+        if left == -1:
+            return right
+        return min(left, right)
