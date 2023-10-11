a53d1d92be34e4a739cfeb36d45b01d68312fcd2
libra146 <1462326016@qq.com>
2023-10-11 10:18:34 +0800

    feature: add er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof.py

---
+++ tree/binary-tree/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof.py
@@ -0,0 +1,23 @@
+from utils import TreeNode
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root:
+            return
+        if root.val == p.val or root.val == q.val:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left is not None and right is not None:
+            return root
+        return left if left is not None else right
+    # leetcode submit region end(Prohibit modification and deletion)
