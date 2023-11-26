37b3a9833ddda4ad925d0c1ed357e3a0a6d58d33
libra146 <1462326016@qq.com>
2023-11-26 21:09:21 +0800

    feature: add delete-leaves-with-a-given-value.py

---
+++ tree/binary-tree/delete-leaves-with-a-given-value.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from utils import TreeNode
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        if not root:
+            return
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+        if not root.left and not root.right and root.val == target:
+            return
+        return root
+# leetcode submit region end(Prohibit modification and deletion)
