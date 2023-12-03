e50c9bda5c30daf61c17edefe767537323379467
libra146 <1462326016@qq.com>
2023-12-03 22:33:10 +0800

    feature: add path-in-zigzag-labelled-binary-tree.py

--- tree/binary-tree/path-in-zigzag-labelled-binary-tree.py
+++ tree/binary-tree/path-in-zigzag-labelled-binary-tree.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 
@@ -13,5 +14,19 @@ class Solution:
         for index in range(len(res)):
             if index % 2 == len(res) % 2 and index != len(res) - 1 and index != 0:
                 res[index] = pow(2, index + 1) - 1 - (res[index] - pow(2, index))
+        return res
+
+
+class Solution2:
+    def pathInZigZagTree(self, label: int) -> List[int]:
+        res = []
+        while label >= 1:
+            res.append(label)
+            label = label // 2
+            if label == 0:
+                continue
+            depth = int(math.log(label) / math.log(2))
+            label = pow(2, depth + 1) - 1 - (label - pow(2, depth))
+        res.reverse()
         return res
 # leetcode submit region end(Prohibit modification and deletion)
