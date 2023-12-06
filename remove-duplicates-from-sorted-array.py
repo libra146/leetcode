8f9b9cd384f5618d11dd54efe277f69e85484f63
libra146 <1462326016@qq.com>
2023-12-06 09:32:01 +0800

    feature: add remove-duplicates-from-sorted-array.py

---
+++ remove-duplicates-from-sorted-array.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        slow = 0
+        fast = 1
+        while fast < len(nums):
+            if nums[slow] != nums[fast]:
+                slow += 1
+                nums[slow] = nums[fast]
+            fast += 1
+        return slow + 1
+
+# leetcode submit region end(Prohibit modification and deletion)
