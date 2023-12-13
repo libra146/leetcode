a0afaf24f1616a4171aa10d0c061be8519c58d4c
libra146 <1462326016@qq.com>
2023-12-13 11:37:40 +0800

    feature: add lexicographically-smallest-palindrome.py

---
+++ lexicographically-smallest-palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def makeSmallestPalindrome(self, s: str) -> str:
+        s = list(s)
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                if s[left] > s[right]:
+                    s[left] = s[right]
+                else:
+                    s[right] = s[left]
+            left += 1
+            right -= 1
+        return ''.join(s)
