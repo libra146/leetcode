f5fb6b0029fdd97b0f6ebca62a9daec93b323803
libra146 <1462326016@qq.com>
2024-01-06 21:54:01 +0800

    feature: add intersection-of-two-arrays.py

---
+++ intersection-of-two-arrays.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
+# leetcode submit region end(Prohibit modification and deletion)
